92cea6a7af9bb6f69e5eadddde5f9c49811ac75a
Manish Prasad <manish_prd@outlook.com>
2020-11-06 21:34:33 +0530

    Add project files.

---
+++ WenSonarCloudProj/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WenSonarCloudProj.Models;
+
+namespace WenSonarCloudProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            bool blnWork = true;
+
+            //if (val == "Good")
+            //    blnWork = true;
+            //else
+            //    blnWork = false;
+
+            if (blnWork)
+                ViewBag.Work = "Good Work";
+            else
+                ViewBag.Work = "Needs Improvement";
+            return View();
+        }
+
+        private readonly string[] whiteList = { "https://www.sonarsource.com" };
+
+        public IActionResult RedirectMe(string url)
+        {
+            return Redirect(url);
+
+            //if (!whiteList.Contains(url))
+            //{
+            //    return BadRequest();
+            //}
+
+            //return Redirect(url);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WenSonarCloudProj/SonarDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WenSonarCloudProj
+{
+    public class SonarDemo
+    {
+        public static void DoSomething()
+        {
+            // Do nothing because of X and Y.
+            //Console.WriteLine("Do Work");
+        }
+        public static void GetGuid(byte[] bytes)
+        {
+            //var g1 = Guid.Empty;
+            //var g2 = Guid.NewGuid();
+            //var g3 = new Guid(bytes);
+
+            var g = new Guid();
+        }
+    }
+}
